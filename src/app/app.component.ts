8751e363ea856c48053772c88341ed333ea89b5b
JesusLopezGomez <jelopez.gom@gmail.com>
2024-06-12 11:21:42 +0200

    portfolio terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavBarComponent,InicioComponent,TecnologiasComponent, ProyectosComponent, ExperienciaComponent, ContactoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
